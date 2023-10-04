5dc708b9fea52221e7f16e117621f00ccc55790a
Zura Doghanadze <zurabi.doghanadze.1@iliauni.edu.ge>
2023-10-04 22:10:32 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import styled from "styled-components"
+
+
+const Main = styled.div `
+  font-size: 40px;
+`
+function App() {
+ 
+  return (
+    <>
+      <Main>
+        hello world
+      </Main>
+    </>
+  )
+}
+
+export default App
